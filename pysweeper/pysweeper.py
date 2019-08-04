e2f3c3e16fd2bf2af731a743f0e8c2125ab7944c
Phillip Cloud <cpcloud@gmail.com>
2019-08-04 10:57:47 -0400

    Add type annotations

--- pysweeper/pysweeper.py
+++ pysweeper/pysweeper.py
@@ -1,147 +1,166 @@
-#!/usr/bin/env python
+"""Sweep some mines, terminal style."""
 
+from typing import List, MutableSet, Optional, Set, Tuple
+
+import collections
+import dataclasses
+import math
 import random
 
-from math import ceil, log10
 
-from collections import deque
-from itertools import product
+Coordinate = Tuple[int, int]
 
 
-class Tile(object):
+@dataclasses.dataclass
+class Tile:
+    """A minesweeper tile."""
 
-    def __init__(self, exposed, flagged, content):
-        self.exposed = exposed
-        self.flagged = flagged
-        self.content = content
+    exposed: bool = False
+    flagged: bool = False
+    content: Optional[int] = None
 
-    def __str__(self):
+    def __str__(self) -> str:
         if self.content is not None:
             return str(self.content)
         if self.exposed:
-            assert not self.flagged, 'Cannot be both exposed and flagged'
+            assert (
+                not self.flagged
+            ), "{} instance cannot be both exposed and flagged".format(
+                type(self).__name__
+            )
         if not self.exposed:
-            return '\u2691' if self.flagged else '\u25A0'
+            return "\u2691" if self.flagged else "\u25A0"
         else:
-            return '\u25A1'
+            return "\u25A1"
 
 
-class Bomb(Tile):
-    def __str__(self):
-        return '\u2600' if self.exposed else super(Bomb, self).__str__()
+@dataclasses.dataclass
+class Mine(Tile):
+    """A minesweeper mine."""
+
+    def __str__(self) -> str:
+        return "\u2600" if self.exposed else super().__str__()
 
 
-def random_points(n, m, k):
-    points = list(product(range(n), range(m)))
+def random_points(n: int, m: int) -> List[Coordinate]:
+    r"""Return a list of coordinates of size :math:`n \times m`."""
+    points = [(x, y) for x in range(n) for y in range(m)]
     random.shuffle(points)
     return points
 
 
-class Grid(object):
+class Grid:
+    """A minesweeper grid."""
 
-    def __init__(self, nrows, ncolumns, bomb_count):
+    def __init__(self, nrows: int, ncolumns: int, mine_count: int) -> None:
         self.nrows = nrows
         self.ncolumns = ncolumns
         self.grid = {
-            (i, j): (
-                Bomb(False, False, None)
-                if k < bomb_count else Tile(False, False, None)
-            )
-            for k, (i, j) in enumerate(
-                random_points(nrows, ncolumns, bomb_count)
-            )
+            point: Mine() if i < mine_count else Tile()
+            for i, point in enumerate(random_points(nrows, ncolumns))
         }
-        self.bomb_count = bomb_count
+        self.mine_count = mine_count
         self.num_flagged = 0
 
-    def __str__(self):
-        nspaces = max(ceil(log10(self.nrows)), ceil(log10(self.ncolumns)))
-        padding = ' ' * nspaces
+    def __len__(self) -> int:
+        return len(self.grid)
+
+    def __str__(self) -> str:
+        nrows = self.nrows
+        ncolumns = self.ncolumns
+        nspaces = max(
+            math.ceil(math.log10(nrows)), math.ceil(math.log10(ncolumns))
+        )
+        padding = " " * nspaces
         rows = [
-            padding + ' '.join(
-                '{:>{}}'.format(i, nspaces + 1) for i in range(self.ncolumns)
+            "{}{}".format(
+                padding,
+                " ".join(str(i).rjust(nspaces + 1) for i in range(ncolumns)),
             )
         ]
-        for i in range(self.nrows):
-            rows.append(
-                '{:>{}}{}'.format(
-                    i,
-                    nspaces,
-                    ' '.join(
-                        padding + str(self.grid[i, j])
-                        for j in range(self.ncolumns)
-                    )
-                )
+        grid = self.grid
+        rows.extend(
+            "{}{}".format(
+                str(i).rjust(nspaces),
+                " ".join(padding + str(grid[i, j]) for j in range(ncolumns)),
             )
-        return '\n'.join(rows)
+            for i in range(nrows)
+        )
+        return "\n".join(rows)
 
-    def adjacent(self, i, j, types=(Tile,)):
+    def adjacent(self, i: int, j: int) -> Set[Coordinate]:
+        """Compute the tiles that are adjacent to the tile at `i`, `j`."""
         increments = 1, -1, 0
-
+        nrows = self.nrows
+        ncolumns = self.ncolumns
         return {
-            (i + x, j + y): self.grid[i + x, j + y]
-            for x, y in product(increments, repeat=2)
-            if (
-                (x or y) and
-                0 <= i + x < self.nrows and
-                0 <= j + y < self.ncolumns and
-                isinstance(self.grid[i + x, j + y], types)
-            )
+            (i + x, j + y)
+            for x in increments
+            for y in increments
+            if x or y
+            if 0 <= i + x < nrows
+            if 0 <= j + y < ncolumns
         }
 
-    def expose(self, i, j):
-        """Exposure algorithm.
+    def expose(self, i: int, j: int) -> int:  # noqa: D213
+        """Tile exposure algorithm.
 
-        Treat the grid as graph, where nodes are tiles and edges are links to
-        immediately adjacent nodes.
+        Treat the grid as graph.
 
-        * If we're a bomb, fail
+        Nodes are tiles, edges are between nodes that are immediately adjacent.
+
+        * If we're a mine, fail
         * Else expose the current tile
-        * Count the number of adajacent tiles that are bombs
+        * Count the number of adajacent tiles that are mines. 
         * If the count is > 0, then replace that tile with a CountedTile
-        2. For every adjacent tile, if any adjacent tiles are bombs, halt
+        2. For every adjacent tile, if any adjacent tiles are mines, halt
         3. Else expose the tile
+
         """
-        if isinstance(self.grid[i, j], Bomb):
-            self.grid[i, j].exposed = True
+        grid = self.grid
+        if isinstance(grid[i, j], Mine):
+            grid[i, j].exposed = True
             return -1
 
-        seen = set()
-        c = deque([((i, j), self.grid[i, j])])
+        seen: MutableSet[Coordinate] = set()
+        coordinates = collections.deque([(i, j)])
         exposed = 0
 
-        while c:
-            (x, y), tile = c.popleft()
-            if (x, y) not in seen:
-                seen.add((x, y))
+        while coordinates:
+            coord = coordinates.popleft()
+            x, y = coord
+            tile = grid[x, y]
+            if coord not in seen:
+                seen.add(coord)
                 adjacent = self.adjacent(x, y)
-                n_adjacent_bombs = sum(
-                    isinstance(t, Bomb) for t in adjacent.values()
+                adjacent_mines = sum(
+                    isinstance(grid[x, y], Mine) for x, y in adjacent
                 )
-                tile_exposed = not isinstance(tile, Bomb)
-                self.grid[x, y].exposed = tile_exposed
+                tile_exposed = not isinstance(tile, Mine)
+                grid[coord].exposed = tile_exposed
                 exposed += tile_exposed
 
-                if n_adjacent_bombs:
-                    self.grid[x, y].content = n_adjacent_bombs
+                if adjacent_mines:
+                    grid[coord].content = adjacent_mines
                 else:
-                    c.extend(
-                        (coord, tile) for coord, tile in adjacent.items()
-                        if coord not in seen
+                    coordinates.extend(
+                        coord for coord in adjacent if coord not in seen
                     )
         return exposed
 
-    def flag(self, i, j):
-        node = self.grid[i, j]
-        was_flagged = node.flagged
-        new_flagged = not was_flagged
+    def flag(self, i: int, j: int) -> int:
+        """Flag the tile at coordinate `i`, `j`."""
+        grid = self.grid
+        tile = grid[i, j]
+        was_flagged = tile.flagged
+        flagged = not was_flagged
 
         if was_flagged:
             if self.num_flagged - 1 >= 0:
-                self.grid[i, j].flagged = new_flagged
+                grid[i, j].flagged = flagged
                 self.num_flagged -= 1
         else:
-            if self.num_flagged + 1 <= self.bomb_count and not node.exposed:
-                self.grid[i, j].flagged = new_flagged
+            if self.num_flagged + 1 <= self.mine_count and not tile.exposed:
+                grid[i, j].flagged = flagged
                 self.num_flagged += 1
-        return False
+        return 0
